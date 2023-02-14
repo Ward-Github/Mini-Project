9c7929c1def3fe806930b3d3eb639995f399bcf6
nezirnezirevic310 <1054620@hr.nl>
2023-02-14 15:42:08 +0100

    errors gefixt

--- Location.cs
+++ Location.cs
@@ -9,6 +9,9 @@
     public Location LocationToEast;
     public Location LocationToSouth;
     public Location LocationToWest;
+    private object value1;
+    private object value2;
+    private object value3;
 
     public Location(int id, string name, string description)
     {
@@ -17,4 +20,14 @@
         Description = description;
         //Hier moeten nog dingen toegevoegd worden
     }
+
+    public Location(int id, string name, string description, object value1, object value2, object value3) : this(id, name, description)
+    {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public Quest QuestAvailableHere { get; internal set; }
+    public Monster MonsterLivingHere { get; internal set; }
 }
