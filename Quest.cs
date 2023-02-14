20ea71f497ab6fed5620e4f1d6b641f008dd4033
fjspaak <0765612@hr.nl>
2023-02-14 15:27:52 +0100

    Create Quest.cs

---
+++ Quest.cs
@@ -0,0 +1,12 @@
+public class Quest
+{
+    public int ID;
+    public string Name;
+    public string Description;
+    public Item ItemRequired;
+    public Quest QuestAvailableHere;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
+}
