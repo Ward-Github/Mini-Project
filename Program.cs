e55b5877bc4ea215a3e3162c165ac41dbab76657
nezirnezirevic310 <1054620@hr.nl>
2023-02-14 15:05:57 +0100

    yo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Halloooooooooooooooo nezir!, doei");
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("bruh");
+    }
+}
