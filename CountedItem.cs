935472751e6b99541b86038f42c2ea364fef2f20
fjspaak <0765612@hr.nl>
2023-02-14 15:19:49 +0100

    Create CountedItem.cs

---
+++ CountedItem.cs
@@ -0,0 +1,11 @@
+public class CountedItem
+{
+    public Item TheItem;
+    public int Quantity;
+
+    public CountedItem(Item theitem, int quantity)
+    {
+        TheItem = theitem;
+        Quantity = quantity;
+    }
+}
