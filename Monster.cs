54f2287eb0fef9402a44c033bf88159a21431849
nezirnezirevic310 <1054620@hr.nl>
2023-02-14 15:48:55 +0100

    monster_id_rat toegevoegd

--- Monster.cs
+++ Monster.cs
@@ -8,6 +8,14 @@
     public int RewardGold;
     public CountedItemList Loot;
     public int CurrenHitPoints;
+    private int mONSTER_ID_RAT;
+    private string v1;
+    private string v2;
+    private int v3;
+    private int v4;
+    private int v5;
+    private int v6;
+    private int v7;
 
     public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperience, int rewardGold,
         CountedItemList loot, int currenHitPoints)
@@ -20,5 +28,17 @@
         RewardGold = rewardGold;
         Loot = loot;
         CurrenHitPoints = currenHitPoints;
+    }
+
+    public Monster(int mONSTER_ID_RAT, string v1, string v2, int v3, int v4, int v5, int v6, int v7)
+    {
+        this.mONSTER_ID_RAT = mONSTER_ID_RAT;
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.v4 = v4;
+        this.v5 = v5;
+        this.v6 = v6;
+        this.v7 = v7;
     }
 }
