eeb707765b5e35b7ae714c6964ddb6fd1b8b6b36
Ward <113605493+Ward-Github@users.noreply.github.com>
2023-02-15 10:14:07 +0100

    ja

--- Monster.cs
+++ Monster.cs
@@ -8,17 +8,10 @@
     public int RewardGold;
     public CountedItemList Loot;
     public int CurrenHitPoints;
-    private int mONSTER_ID_RAT;
-    private string v1;
-    private string v2;
-    private int v3;
-    private int v4;
-    private int v5;
-    private int v6;
-    private int v7;
+    public int MaximumHitPoints;
 
     public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperience, int rewardGold,
-        int loot, int currenHitPoints)
+        int loot, int currenHitPoints, int maximumHitPoints)
     {
         ID = id;
         Name = name;
@@ -26,19 +19,8 @@
         MaximumDamage = maximumDamage;
         RewardExperience = rewardExperience;
         RewardGold = rewardGold;
-        Loot = new CountedItemList(new CountedItem(loot));
+        MaximumHitPoints = maximumDamage;
         CurrenHitPoints = currenHitPoints;
     }
-
-    public Monster(int mONSTER_ID_RAT, string v1, string v2, int v3, int v4, int v5, int v6, int v7)
-    {
-        this.mONSTER_ID_RAT = mONSTER_ID_RAT;
-        this.v1 = v1;
-        this.v2 = v2;
-        this.v3 = v3;
-        this.v4 = v4;
-        this.v5 = v5;
-        this.v6 = v6;
-        this.v7 = v7;
-    }
+    
 }
