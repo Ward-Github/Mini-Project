7e4dc1df92a64b85ad54fad4c57a63fb33ba9c02
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-02-15 10:32:16 +0100

    fix location

--- Location.cs
+++ Location.cs
@@ -4,30 +4,21 @@
     public string Name;
     public string Description;
     public Item ItemRequiredToEnter;
-    public Quest QuestAvilableHere;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
     public Location LocationToNorth;
     public Location LocationToEast;
     public Location LocationToSouth;
     public Location LocationToWest;
-    private object value1;
-    private object value2;
-    private object value3;
 
-    public Location(int id, string name, string description)
+    public Location(int id, string name, string description, Item itemRequiredToEnter, Quest questAvailableHere, Monster monsterLivingHere)
     {
         ID = id;
         Name = name;
         Description = description;
-        //Hier moeten nog dingen toegevoegd worden
+        ItemRequiredToEnter = itemRequiredToEnter;
+        QuestAvailableHere = questAvailableHere;
+        MonsterLivingHere = monsterLivingHere;
     }
-
-    public Location(int id, string name, string description, object value1, object value2, object value3) : this(id, name, description)
-    {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
-    }
-
-    public Quest QuestAvailableHere { get; internal set; }
-    public Monster MonsterLivingHere { get; internal set; }
+    
 }
