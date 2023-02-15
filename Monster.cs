530ea1548ba83eac340b3f8504f804754431e00b
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-02-15 14:59:45 +0100

    added monster battle

--- Monster.cs
+++ Monster.cs
@@ -1,4 +1,6 @@
-﻿public class Monster
+﻿using System.Data;
+
+public class Monster
 {
     public int ID;
     public string Name;
@@ -7,11 +9,11 @@
     public int RewardExperience;
     public int RewardGold;
     public CountedItemList Loot;
-    public int CurrenHitPoints;
+    public int CurrentHitPoints;
     public int MaximumHitPoints;
 
     public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperience, int rewardGold,
-        int currenHitPoints, int maximumHitPoints)
+        int currentHitPoints, int maximumHitPoints)
     {
         ID = id;
         Name = name;
@@ -19,9 +21,39 @@
         MaximumDamage = maximumDamage;
         RewardExperience = rewardExperience;
         RewardGold = rewardGold;
-        MaximumHitPoints = maximumDamage;
-        CurrenHitPoints = currenHitPoints;
+        CurrentHitPoints = currentHitPoints;
+        MaximumHitPoints = maximumHitPoints;
         Loot = new CountedItemList();
     }
 
+    public bool BossFight(Player player)
+    {
+        //player naam en health toevoegen
+        Console.WriteLine($@"You have come here to defeat the {Name}
+player Health: idk
+{Name} Health: {MaximumHitPoints}");
+        while (CurrentHitPoints > 0)
+        {
+            if (player.CurrentHitPoints > 0)
+            {
+                Random random = new Random();
+                int PlayerDamage = random.Next(player.CurrentWeapon.MinimumDamage, player.CurrentWeapon.MaximumDamage);
+                int MonsterDamage = random.Next(0, MaximumDamage);
+                CurrentHitPoints -= PlayerDamage;
+                player.CurrentHitPoints -= MonsterDamage;
+                Console.WriteLine($@"Current Weapon: {player.CurrentWeapon.Name}
+{Name} hit for {PlayerDamage} HP
+You were hit for {MonsterDamage} HP");
+                Thread.Sleep(50);
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("You died!");
+                return false;
+            }
+        }
+        Console.WriteLine("You win!");
+        return true;
+    }
 }
